05067fa3da27addc6826c44d26f72fe538f1e8ca
Douglas Oliveira <63203428+imdsoliveira@users.noreply.github.com>
2025-02-01 14:43:33 -0300

    update

--- get_profile.py
+++ get_profile.py
@@ -3,9 +3,6 @@ import instaloader
 # Cria uma instância do Instaloader
 loader = instaloader.Instaloader()
 
-# Faz login com suas credenciais (opcional, mas recomendado para acessar perfis privados)
-# loader.login('seu_usuario', 'sua_senha')
-
 # Define o nome de usuário do perfil desejado
 username = 'dracintiacardoso'
 
